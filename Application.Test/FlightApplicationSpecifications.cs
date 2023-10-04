78a3ada38bb7da7d82c96cd12731230a8d8aa47b
Marcello Cavazza <marcellocavazzaoliveira.dev@gmail.com>
2023-10-04 00:03:59 -0300

    commiting

--- Application.Test/FlightApplicationSpecifications.cs
+++ Application.Test/FlightApplicationSpecifications.cs
@@ -1,3 +1,5 @@
+using FluentAssertions;
+
 namespace Application.Test
 {
     public class FlightApplicationSpecifications
@@ -5,7 +7,35 @@ namespace Application.Test
         [Fact]
         public void Books_Flights()
         {
+            BookingService bookingService = new();
+            bookingService.Book(new BookDTO());
+            bookingService.FindBookings().Should().ContainEquivalentOf(
+                new BookingRM()
+                );
+        }
+
+    }
+
+    public class BookDTO
+    {
+
+    }
+
+    public class BookingService
+    {
+        public void Book(BookDTO data)
+        {
 
         }
+
+        public IEnumerable<BookingRM> FindBookings()
+        {
+
+        }
+    }
+
+    public class BookingRM
+    {
+
     }
 }
